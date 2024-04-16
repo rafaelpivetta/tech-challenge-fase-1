eecb183bfc4eade6be5e08fee1b634f68b47f75d
Rafael Pivetta Balbuena <rafael.pivetta@gmail.com>
2024-04-15 23:01:57 -0300

    feat: Aula 4 de ML (Feature Scaling)

--- previsao.py
+++ previsao.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
@@ -25,6 +26,21 @@ y = dataset_tratado['encargos']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+#Normalização
+scaler = MinMaxScaler() #Normalizacao dos dados (0-1)
+
+scaler.fit(X_train)
+x_train_min_max_scaled = scaler.transform(X_train)
+x_test_min_max_scaled= scaler.transform(X_test)
+
+
+#Padronização
+scaler = StandardScaler()
+
+scaler.fit(X_train)
+x_train_standard_scaled = scaler.transform(X_train)
+x_test_standard_scaled  = scaler.transform(X_test)
+
 def evaluate_model(y_test, predictions):
     mse = mean_squared_error(y_test, predictions)
     rmse = np.sqrt(mse) 
@@ -44,7 +60,19 @@ def run_model(model, X_train, y_train, X_test):
     print(model)
     evaluate_model(y_test, predictions)
 
+
 run_model(LinearRegression(), X_train, y_train, X_test)
+run_model(LinearRegression(), x_train_min_max_scaled, y_train, x_test_min_max_scaled)
+run_model(LinearRegression(), x_train_standard_scaled, y_train, x_test_standard_scaled)
+
 run_model(DecisionTreeRegressor(), X_train, y_train, X_test)
-run_model(RandomForestRegressor(), X_train, y_train, X_test)    
+run_model(DecisionTreeRegressor(), x_train_min_max_scaled, y_train, x_test_min_max_scaled)
+run_model(DecisionTreeRegressor(), x_train_standard_scaled, y_train, x_test_standard_scaled)
+
+run_model(RandomForestRegressor(), X_train, y_train, X_test)
+run_model(RandomForestRegressor(), x_train_min_max_scaled, y_train, x_test_min_max_scaled)
+run_model(RandomForestRegressor(), x_train_standard_scaled, y_train, x_test_standard_scaled)
+
 run_model(GradientBoostingRegressor(), X_train, y_train, X_test)
+run_model(GradientBoostingRegressor(), x_train_min_max_scaled, y_train, x_test_min_max_scaled)
+run_model(GradientBoostingRegressor(), x_train_standard_scaled, y_train, x_test_standard_scaled)
